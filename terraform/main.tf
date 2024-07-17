fcb55bcd35b35c8b421e1cfc858f323acd9d3acc
Charles <charlesnwoye2@gmail.com>
2024-07-17 07:48:40 +0100

    Terraform GitHub Actions setup

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "=3.0.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-cloudresumeapi"
+    storage_account_name = "stcloudresumeapi"
+    container_name       = "stcontainercloudresumeapi"
+    key                  = "dev.terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
