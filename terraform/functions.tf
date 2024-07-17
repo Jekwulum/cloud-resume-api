4508832adc8ed6fa85bf7bb9c948b122d0fe012b
Charles <charlesnwoye2@gmail.com>
2024-07-17 01:29:18 +0100

    Terraform cloud setup - Azure resources setup for cloudresumeapi successful - Resources: Resource Group, Storage Account, Functions

---
+++ terraform/functions.tf
@@ -0,0 +1,32 @@
+resource "azurerm_storage_account" "main" {
+  name                     = "st${var.application_name}"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_service_plan" "main" {
+  name                = "asp${var.application_name}"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  os_type             = "Linux"
+  sku_name            = "Y1"
+}
+
+resource "azurerm_linux_function_app" "main" {
+  name                = "function${var.application_name}"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+
+  storage_account_name       = azurerm_storage_account.main.name
+  storage_account_access_key = azurerm_storage_account.main.primary_access_key
+  service_plan_id            = azurerm_service_plan.main.id
+  depends_on                 = [azurerm_service_plan.main, azurerm_storage_account.main]
+
+  site_config {}
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "application_name" {
+  description = "The name of the application"
+  type        = string
+}
+
+variable "location" {
+  description = "The location of the resources"
+  type        = string
+}
