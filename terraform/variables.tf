e1a5c73e45cbdec03cd8d7e06b39e0a64cf06595
Charles <charlesnwoye2@gmail.com>
2024-07-17 08:02:30 +0100

    Terraform GitHub Actions setup - Added default to variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,11 @@
 variable "application_name" {
   description = "The name of the application"
   type        = string
+  default     = "cloudresumeapi"
 }
 
 variable "location" {
   description = "The location of the resources"
   type        = string
+  default     = "East US"
 }
