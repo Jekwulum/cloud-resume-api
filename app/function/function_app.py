e2d963801d5254b62bcf1a35828a5b5a42404f8f
Charles <charlesnwoye2@gmail.com>
2024-07-22 01:47:03 +0100

    Terraform GitHub Actions and cosmosdb setup - added config for cosmos mongodb - deployment.yaml import existing resource

--- app/function/function_app.py
+++ app/function/function_app.py
@@ -3,8 +3,8 @@ import logging
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
-@app.route(route="reume_handler")
-def reume_handler(req: func.HttpRequest) -> func.HttpResponse:
+@app.route(route="resume")
+def resume(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     name = req.params.get('name')
